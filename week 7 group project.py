6332aeb8c88c6159d90dc6b21a56c859fcca68da
adsean1 <104611238+adsean1@users.noreply.github.com>
2023-04-06 11:36:59 +0930

    Update week 7 group project.py

--- week 7 group project.py
+++ week 7 group project.py
@@ -1,1 +1,10 @@
 #git@github.com:ALLVIS-II/Project-week-7.git
+
+item_List = []
+item = input("Enter an item('STOP' to quit):")
+while item.upper() != 'STOP':
+    while item.capitalize() in item_List:
+        print('Error:', item, 'already entered.')
+        item = input('Enter another item:')
+
+if item.upper() != 'STOP':
